8cfe91d207b3f7a1b757a523b94cf8483f95bd07
DESKTOP-FK89NF0\hgh04 <gbwebdeveloper0425@gmail.com>
2022-12-20 14:13:58 +0900

    아주 간단한 회원관리 서비스 제작 + 테스트 (feat 람다 사용법 학습완료)

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -22,7 +22,6 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Optional<Member> findByName(String name) {
-
         return store.values().stream()
                 .filter(member -> member.getName().equals(name))
                 .findAny();
